08a5b6ca2177649b25d8cf4b59931e6792df04c1
muhafriza <muhafriza49@gmail.com>
2025-02-02 14:08:57 +0700

    Route

--- monitoring-app/src/component/util/CreateRoute.jsx
+++ monitoring-app/src/component/util/CreateRoute.jsx
@@ -16,7 +16,7 @@ const LaporanKerusakan = lazy(() => import ("../page/laporan-kerusakan-teknisi/R
 const MasterMesin = lazy(() => import ("../page/master-mesin/Root"));
 const RiwayatPerawatanPreventif = lazy(() => import ("../page/riwayat_perawatan_preventif/Root"));
 
-const KorektifPic = lazy(() => import ("../page/transaksi-korektif/Root"));
+const KorektifPic = lazy(() => import ("../page/transaksi-korektif-pic/Root"));
 const KorektifTeknisi = lazy(() => import ("../page/transaksi-korektif-teknisi/Root"));
 
 
